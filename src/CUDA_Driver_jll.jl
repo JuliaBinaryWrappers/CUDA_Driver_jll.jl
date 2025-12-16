039474600cf989a464f195dc573326e8cfa6a6cc
jlbuild <juliabuildbot@gmail.com>
2025-12-16 13:23:35 +0000

    CUDA_Driver_jll build 13.1.0+0

--- src/CUDA_Driver_jll.jl
+++ src/CUDA_Driver_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("CUDA_Driver")
-JLLWrappers.@generate_main_file("CUDA_Driver", UUID("4ee394cb-3365-5eb0-8335-949819d2adfc"))
+JLLWrappers.@generate_main_file("CUDA_Driver", Base.UUID("4ee394cb-3365-5eb0-8335-949819d2adfc"))
 end  # module CUDA_Driver_jll
